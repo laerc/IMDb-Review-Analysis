4bd34d6aa2eb0fb9b8df690b710956eb45d4acd8
laerc <laercio00@outlook.com>
2018-09-18 23:18:17 -0300

    added function to collect data

--- main.py
+++ main.py
@@ -1,28 +1,51 @@
 # How to get an API key? http://www.omdbapi.com/apikey.aspx
+# Get reviews from an API? https://developers.themoviedb.org/3/reviews/get-review-details
+# Dataset : http://ai.stanford.edu/%7Eamaas/data/sentiment/
+# To start, just download the dataset, and extract the file in the same folder of the main.py
 
 import omdb
 import json
+import os
 
-import requests
-import bs4
+#debug print and return the movie info
+def get_imdb_info(id, type='movie'):
+	API_KEY = "68735ac"
 
-API_KEY = "68735ac"
+	omdb.set_default('apikey', API_KEY)
+	omdb.set_default('tomatoes', True)
 
-omdb.set_default('apikey', API_KEY)
-omdb.set_default('tomatoes', True)
+	movie = omdb.imdbid(id, fullplot=True, tomatoes=True)
 
-tv_show_name = "deadpool 2"
+	url = 'https://www.imdb.com/title/' + movie['imdb_id'] + '/'
+	print (url)
+	print (json.dumps(movie, indent=4, sort_keys=True))
+	#return and info about the movie
+	return movie
 
-movie = omdb.get(title=tv_show_name, fullplot=True, media_type="movie")
+#get the files from a directory structure
+def get_files_name(pattern='train'):
+	files_name = []
 
-url = 'https://www.imdb.com/title/' + movie['imdb_id'] + '/'
-print (url)
+	for dirname, dirnames, filenames in os.walk('.'):
 
-'''
-source_code = requests.get(url)
-plain_text = source_code.text
-soup = bs4.BeautifulSoup(plain_text, 'html.parser')
-print (soup)
-'''
+		# print path to all subdirectories first.
+		for subdirname in dirnames:
+			print(os.path.join(dirname, subdirname))
+		
+		#garantees the pattern in the name and get only usefull data
+		if pattern in dirname and len(dirnames) == 0:
+			# print path to all filenames.
+			for filename in filenames:
+				files_name.append(filename)
+				#print(os.path.join(dirname, filename))
 
-print (json.dumps(movie, indent=4, sort_keys=True))
+		# Advanced usage:
+		# editing the 'dirnames' list will stop os.walk() from recursing into there.
+		if '.git' in dirnames:
+		# don't go into any .git directories.
+			dirnames.remove('.git')
+
+	return files_name
+
+get_imdb_info('tt0010790')
+files_name = get_files_name('train')
