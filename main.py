ce3e7013013266bf49a47d8bddde5c07b955d3bc
laerc <laercio00@outlook.com>
2018-09-26 09:50:04 -0300

    Adding ngrams parser

--- main.py
+++ main.py
@@ -5,10 +5,11 @@
 
 import os
 import re
-import json
-import omdb
 import bs4
+import json
 import nltk
+import omdb
+
 from nltk.corpus import stopwords
 
 #Class that holds information about the each review
@@ -31,7 +32,7 @@ class Review:
 
 		return id
 
-	def clean_string(self):
+	def remove_html_tags(self):
 		text = bs4.BeautifulSoup(self.review, "lxml")
 		return text.get_text()
 
@@ -51,6 +52,27 @@ class Review:
 		#return and info about the movie
 		return movie
 
+	def parse_data(self, review, path_to_file, sw):
+		print(path_to_file)
+		parsed_string = re.split('[_ |.]',self.path_to_file[1])
+		
+		id, rating = self.format_id(parsed_string[0]), int(parsed_string[1])
+		#remove all the html tags
+		self.review = self.remove_html_tags()
+		#tokenize the words and remove all the non alpha characters
+		self.review = self.tokenize_string()
+
+		#remove stop words
+		self.review = self.remove_stop_words()
+
+		
+		print (id, rating)
+
+		return {'id': id, 'rating': rating, 'review': review}
+
+	def remove_stop_words(self):
+		return [word for word in self.review if word not in self.sw]
+
 	# This method apply tokenize to the string
 	def tokenize_string(self):
 		# tokenize the string
@@ -58,26 +80,9 @@ class Review:
 		tokens = [word.lower() for word in tokens if word.isalpha()]
 		return tokens
 
-	def remove_stop_words(self):
-		return [word for word in self.review if word not in self.sw]
-
-	def parse_data(self, review, path_to_file, sw):
-		print(path_to_file)
-		parsed_string = re.split('[_ |.]',self.path_to_file[1])
-		
-		id, rating = self.format_id(parsed_string[0]), int(parsed_string[1])
-		#remove all the html tags
-		self.review = self.clean_string()
-		#tokenize the words and remove all the non alpha characters
-		self.review = self.tokenize_string()
-
-		self.review = self.remove_stop_words()
-
-		#remove steop words
-		print (id, rating)
-
-		return {'id': id, 'rating': rating, 'review': review}
-
+	def generate_ngrams(self, n=1):
+		ngrams = nltk.ngrams(self.review,n)
+		return ngrams
 
 class Parser:
 	pattern = ''
@@ -118,6 +123,7 @@ class Parser:
 			with open(path[0] + '/' + path[1]) as reader:
 				review = reader.read()
 				review = Review(review, path)
+				review.generate_ngrams(n=3)
 				reviews.append(review)
 		return reviews
 
